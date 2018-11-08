18e537c11b31d8638d3aba3ec370eb40565e9afe
nsbb <memorychib@gmail.com>
2018-11-08 16:31:55 +0900

    Update change_color.sh

--- change_color.sh
+++ change_color.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo PS1="\[\e[01;91m\]\u\[\e[m\]\[\e[01;93m\]@\[\e[m\]\[\e[01;92m\]\h\[\e[m\]\[\e[01;97m\]:\[\e[m\]\[\e[01;94m\]\w\[\e[m\]\[\e[01;97m\]$\[\e[m\] "' >> ~/.bashrc
+echo 'PS1="\[\e[01;91m\]\u\[\e[m\]\[\e[01;93m\]@\[\e[m\]\[\e[01;92m\]\h\[\e[m\]\[\e[01;97m\]:\[\e[m\]\[\e[01;94m\]\w\[\e[m\]\[\e[01;97m\]$\[\e[m\] "' >> ~/.bashrc
 source ~/.bashrc
 git clone https://github.com/dracula/vim.git
 sudo mv ./vim/colors/dracula.vim /usr/share/vim/vim80/colors/
